96fac19798e9da3a6311aba9caaa3e67e26a8a34
Karel Minarik <karmi@karmi.cz>
2010-02-14 16:21:02 +0100

    Converted dots to tiny rectangles to represent revisions

--- application.js
+++ application.js
@@ -33,15 +33,14 @@ function WikipediaHistory(data) {
         leftgutter = 30,
         bottomgutter = 80,
         topgutter = 20,
-        dot_color = '#004cbf',
-        dot_diameter = 4,
-        dot_stroke = 1,
+        marker_width = 20,
+        marker_height = 3,
+        marker_stroke = 1,
         r = Raphael("holder", width, height),
         txt_white  = {font: '12px Arial', fill: "#fff"},
         txt_normal = {font: '12px Arial', fill: "#000"},
         txt_small  = {font: '9px Arial', fill: "#666"};
-    var path = r.path().attr({stroke: dot_color, "stroke-width": 4, "stroke-linejoin": "round"}),
-        frame = r.rect(10, 10, 100, 40, 5).attr({fill: "#000", stroke: "none"}).hide(),
+    var frame = r.rect(10, 10, 100, 40, 5).attr({fill: "#000", stroke: "none"}).hide(),
         leave_timer,
         label = [],
         is_label_visible = false,
@@ -54,14 +53,15 @@ function WikipediaHistory(data) {
     // Draw year and month marks
     var draw_year = function(year, x) {
       var y = height - bottomgutter + 60;
-      var text = r.text(x, y, year).attr({"fill":"#999", "font-size":32}).toBack();
-      r.path(["M", x+(dot_diameter*1.5)-text.getBBox().width/2, y-20, "L", x+(dot_diameter*1.5)-text.getBBox().width/2, y-60]).attr({"stroke":"#999"}).toBack();
+      var x = Math.round( x + (marker_width/2) - 4 );
+      var text = r.text(x+18, y, year).attr({"fill":"#999", "font-size":32}).toBack();
+      r.path(["M", x+(marker_width)-text.getBBox().width/2, y-20, "L", x+(marker_width)-text.getBBox().width/2, y-60]).attr({"stroke":"#999"}).toBack();
     };
     var draw_month = function(month_index, x) {
       var month_names = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
       var y = height - bottomgutter + 10;
       var content = month_names[month_index];
-      r.text(x-leftgutter+12, y, content).attr({"fill":"#999", "font-size":8}).rotate(45).toBack();
+      r.text(x-leftgutter+10, y+2, content).attr({"fill":"#999", "font-size":8}).rotate(45).toBack();
     };
 
     // Draw line at graph base
@@ -69,7 +69,7 @@ function WikipediaHistory(data) {
 
     // Draw first year
     var current_year = new Date($this.data[0].date).getFullYear();
-    draw_year(current_year, leftgutter*2);
+    draw_year(current_year, leftgutter+10);
 
     // Iterate over months
     for (var i = 0, ii = $this.data.length; i < ii; i++) {
@@ -79,7 +79,7 @@ function WikipediaHistory(data) {
       // Draw year mark
       var year = new Date($this.data[i].date).getFullYear();
       if (year > current_year) {
-        draw_year(year, x);
+        draw_year(year, x-marker_width);
         current_year = year;
       }
 
@@ -93,40 +93,40 @@ function WikipediaHistory(data) {
       // Iterate over revisions in month
       $.each( data[i].revisions, function(rev_index, rev) {
         total_revision_count += 1;
-        // var x = leftgutter + (20 * i);
-        // var x = Math.round(leftgutter + (2*dot_diameter*rev_index) + dot_stroke + move_by);
 
         var x = leftgutter + (20 * i);
-        var y = Math.round(height - bottomgutter - (2*dot_diameter*rev_index) - dot_stroke - move_by);
+        var y = Math.round(height - bottomgutter - ((2*marker_height)*rev_index) - move_by);
 
         // Colorize dot according to revision impact
-        if (rev.impact > 1) { var dot_color = 'green'; } else if (rev.impact < -1) { var dot_color = 'red'; } else { var dot_color = '#004cbf'; }
+        if      (rev.impact > 1)  { var marker_color = 'green'; }
+        else if (rev.impact < -1) { var marker_color = 'red'; }
+        else                      { var marker_color = '#004cbf'; }
 
-        // Draw revision dot
-        var dot = r.circle(x, y, dot_diameter)
-                   .attr({fill: dot_color, stroke: "#fff", "stroke-width": dot_stroke});
+        // Draw revision marker
+        var marker = r.rect(x, y, marker_width, marker_height)
+                   .attr({fill: marker_color, stroke: "#fff", "stroke-width": marker_stroke});
         if ( rev.impact_ratio && (rev.impact_ratio < 0 || rev.impact_ratio > 0) ) {
           ratio = Math.abs(rev.impact_ratio)+1;
           if (ratio > 5) ratio = 5;
-          dot.scale(ratio, ratio);
-          dot.translate( 0, -Math.round( (dot.getBBox().height - (2*dot_diameter+dot_stroke)) / 2 ) );
+          // marker.scale(ratio, ratio);
+          // marker.translate( 0, -Math.round( (marker.getBBox().height - (2*marker_height+marker_stroke)) / 2 ) );
         }
-        move_by += Math.round(dot.getBBox().height - (dot_diameter*2));
+        move_by += Math.round(marker.getBBox().height - (marker_height*2));
 
         // Add dot handlers
-        $(dot.node)
+        $(marker.node)
           .hover(
             function () {
-              var newcoord = { x: dot.getBBox().x, y: dot.getBBox().y+(dot.getBBox().height) };
+              var newcoord = { x: marker.getBBox().x, y: marker.getBBox().y+(marker.getBBox().height) };
               if (newcoord.x + 100 > width) { newcoord.x -= 114; };
               frame.attr({x: newcoord.x, y: newcoord.y}).show();
               label[0].attr({text: rev.author, x: +newcoord.x + 50, y: +newcoord.y + 12}).show();
               label[1].attr({text: rev.date, x: +newcoord.x + 50, y: +newcoord.y + 27}).show();
-              dot.attr({fill: '#000'});
+              marker.attr({fill: '#000'});
               is_label_visible = true;
             },
             function () {
-              dot.attr({fill: dot_color});
+              marker.attr({fill: marker_color});
               frame.hide();
               label[0].hide();
               label[1].hide();
@@ -148,7 +148,8 @@ function WikipediaHistory(data) {
     blanket.toFront();
    };
 
-   r.text(leftgutter+60, height-bottomgutter+12, 'Displaying ' + total_revision_count + ' revisions').attr({fill: "#b2b2b2"});
+   // Display total number of displayed revisions
+   r.text(width-80, height-bottomgutter+70, 'Displaying ' + total_revision_count + ' revisions').attr({fill: "#b2b2b2"}).toBack();
   };
 
 };
